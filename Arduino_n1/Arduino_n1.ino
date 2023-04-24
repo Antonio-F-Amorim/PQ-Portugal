7d9e79131ce027781e3fbe803319c35b41a4c517
antoniofilipeamorim@gmail.com <antoniofilipeamorim@gmail.com>
2023-04-24 21:07:32 +0100

    corrections for the Alley and telma animations+sensors to work

--- Arduino_n1/Arduino_n1.ino
+++ Arduino_n1/Arduino_n1.ino
@@ -9,18 +9,19 @@
 // Led objects initialization
 Adafruit_NeoPixel strip(150,1, NEO_GRB + NEO_KHZ800);
 
-Anim AnimTelma(&Telma,0,White,White,corTelma,60,100,&strip);
-Anim AnimApagaTelma(&Telma,0,White,corTelma,White,300,300,&strip);
-Anim AnimBeco(&Beco,0,White,White,corBeco,60,100,&strip);
-Anim AnimApagaBeco(&Beco,0,White,corBeco,White,100,100,&strip);
+Anim AnimTelma(&Telma,0,White,White,corTelma,30,150,&strip);
+Anim AnimApagaTelma(&Telma,0,White,corTelma,White,50,50,&strip);
+Anim AnimBeco(&Beco,0,White,White,corBeco,30,80,&strip);
+Anim AnimApagaBeco(&Beco,0,White,corBeco,White,30,30,&strip);
 
-Anim AnimCoracao(&Coracao,0,White,White,corCoracao,60,60,&strip);
-Anim AnimApagaCoracao(&Coracao,0,White,corCoracao,White,100,100,&strip);
+Anim AnimCoracao(&Coracao,0,White,White,corCoracao,30,30,&strip);
+Anim AnimApagaCoracao(&Coracao,0,White,corCoracao,White,30,30,&strip);
 
 Anim2 totalBeco(&AnimBeco,&AnimCoracao);
 Anim2 totalApagaBeco(&AnimApagaBeco,&AnimApagaCoracao);
 
 
+tempo n1Timer(80);
 
 // sensor objects initialization
 remRead2 reciever;
@@ -29,17 +30,18 @@ sensorRemoto sensorTelma(&reciever,1),sensorBeco(&reciever,3);
 
 void setup() {
   startAll(14,White.toUint32(),&strip);
-  delay(1000);
+  delay(2000);
 }
+
 
 
 void loop() {
 
-
+n1Timer.start();
 
 reciever.update();
 
- if(!AnimTelma.runStep()&&!AnimApagaTelma.runStep()){ 
+if(!AnimTelma.runStep()&&!AnimApagaTelma.runStep()){ 
 
    if(sensorTelma.checkSensor()) {
      if(!AnimTelma.justFinished){ 
@@ -67,9 +69,20 @@ reciever.update();
       totalApagaBeco.begin();
       totalBeco.justFinished=false;
     }
+  }*//*
+ if(!AnimBeco.runStep()&&!AnimApagaBeco.runStep()){ 
+
+    if(sensorBeco.checkSensor()){
+     if(!AnimBeco.justFinished){ 
+        AnimBeco.begin();
+        AnimBeco.justFinished=true;
+     } else {
+       AnimApagaBeco.begin();
+       AnimBeco.justFinished=false;
+     }
+   }
   }*/
-  reciever.update();
-  if(!totalBeco.runStep()&&!totalApagaBeco.runStep()){ 
+ if(!totalBeco.runStep()&&!totalApagaBeco.runStep()){ 
 
     if(sensorBeco.checkSensor()){
      if(!totalBeco.justFinished){ 
@@ -82,5 +95,5 @@ reciever.update();
    }
   }
 
-delay(30);
+n1Timer.correctedDelay();
 }
--- Arduino_n2/Arduino_n2.ino
+++ Arduino_n2/Arduino_n2.ino
@@ -11,19 +11,22 @@ Adafruit_NeoPixel strip(150,1, NEO_GRB + NEO_KHZ800);
 
 
 
-Anim AnimTelma(&Telma,0,White,White,corTelma,60,100,&strip);
-Anim AnimApagaTelma(&Telma,0,White,corTelma,White,300,300,&strip);
-Anim AnimBeco(&Beco,0,White,White,corBeco,60,100,&strip);
-Anim AnimApagaBeco(&Beco,0,White,corBeco,White,100,100,&strip);
+Anim AnimTelma(&Telma,0,White,White,corTelma,30,150,&strip);
+Anim AnimApagaTelma(&Telma,0,White,corTelma,White,50,50,&strip);
+Anim AnimBeco(&Beco,0,White,White,corBeco,30,80,&strip);
+Anim AnimApagaBeco(&Beco,0,White,corBeco,White,30,30,&strip);
+
+tempo n2Timer(80);
 
 remWrite2 sender;
-sensorParteLocal sensorTelma(A0,A1,2000,4500,&sender,1),sensorBeco(A4,A5,2000,8000,&sender,3);
+sensorParteLocal sensorTelma(A0,A1,2000,4650,&sender,1),sensorBeco(A4,A5,2000,8000,&sender,3);
 //sensorParteLocal sensorTelma(A0,A1,2000,8000),sensorBeco(A4,A5,2000,8000);
 
 void setup() {
   startAll(12,White.toUint32(),&strip);
-  delay(1000);
   digitalWrite(12,LOW);
+  delay(1000);
+  
 
 }
 
@@ -31,6 +34,7 @@ void setup() {
 
 void loop() {
 
+ n2Timer.start();
  if(!AnimTelma.runStep()&&!AnimApagaTelma.runStep()){ 
 
    if(sensorTelma.checkSensor()) {
@@ -66,13 +70,14 @@ void loop() {
      if(!AnimBeco.justFinished){ 
         AnimBeco.begin();
         AnimBeco.justFinished=true;
+        sender.clear(3);
      } else {
        AnimApagaBeco.begin();
        AnimBeco.justFinished=false;
+       sender.clear(3);
      }
    }
   }
 
-    
-delay(30);
+n2Timer.correctedDelay();
 }
