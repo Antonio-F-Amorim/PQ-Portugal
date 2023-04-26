c3d6d4613e425229b54667e490403fae3096584a
antoniofilipeamorim@gmail.com <antoniofilipeamorim@gmail.com>
2023-04-26 22:42:09 +0100

    sketch to drive the clouds motors

---
+++ nuvem_4/nuvem_4.ino
@@ -0,0 +1,85 @@
+#include<PCA9685.h>
+
+PCA9685 pwmController(0x7F);
+
+class motor {
+ public: 
+  uint8_t MID;
+  double posicao;
+  uint8_t minimo;
+  uint8_t maximo;
+  uint8_t step;
+  bool direc;
+
+  motor(uint8_t mid,float pos,uint8_t min,uint8_t max,uint8_t stp,bool dir){
+    MID=mid;
+    posicao=pos;
+    minimo=min;
+    maximo=max;
+    step=stp;
+    direc=dir;
+  }
+
+  void moveServo( double y){
+
+     if(MID<0||MID>16||y<0||y>100) return;
+    double PWM = y*401/100+105;
+    pwmController.setChannelPWM(MID,PWM);
+    
+  //  Serial.print(" set channel ");
+  //  Serial.print(MID);
+  //  Serial.print(" to ");
+  //  Serial.println(PWM);
+  }
+
+  void incrementMove(){
+    if(direc) {
+      posicao += (double)step/1000;
+      if (posicao>= maximo) {
+        posicao=maximo; direc=!direc;
+      } 
+    }
+    else {
+      posicao -= (double) step/1000;
+      if (posicao<=minimo) {
+        posicao=minimo; direc=!direc;
+      } 
+    }
+    moveServo(posicao);
+    
+  }
+};
+
+  motor motor1(0,50,43,56,100,1);
+  motor motor2(1,50,43,56,80,1);
+  motor motor3(3,50,43,56,90,1);
+  motor motor4(4,50,43,56,110,1);
+
+
+
+//pwmController.setChannelPWM(x-1,PWM);
+
+void setup() {
+  
+  Wire.begin();
+
+  pwmController.resetDevices();       // Resets all PCA9685 devices on i2c line
+
+  pwmController.init();               // Initializes module using default totem-pole driver mode, and default disabled phase balancer
+
+  pwmController.setPWMFrequency(49);
+
+  Serial.begin(9600);
+
+  delay(1000);
+
+}
+
+
+void loop() {
+motor1.incrementMove();
+motor2.incrementMove();
+motor3.incrementMove();
+motor4.incrementMove();
+delay(15);
+}
